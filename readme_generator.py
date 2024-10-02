b20fca8437d3339c4a7c379841e7cf0c38826f3f
Arjun <arjunprakash027@gmail.com>
2024-10-02 12:28:54 +0530

    readme

---
+++ readme_generator.py
@@ -0,0 +1,40 @@
+import os
+from pathlib import Path
+from typing import List
+
+def generate_markdown(directory:Path,ignore_files:List[str],level=0):
+    
+    markdown_lines = []
+
+    for item in sorted(directory.iterdir()):
+        
+        if item.name in ignore_files:
+            continue
+        
+        indent = "  " * level
+
+        if item.is_dir():
+            markdown_lines.append(f"{indent} - **{item.name}**")
+            markdown_lines.extend(generate_markdown(directory=item,ignore_files=ignore_files,level=level + 1))
+        
+        else:
+            markdown_lines.append(f"{indent} - {item.name}")
+    return markdown_lines
+
+def traverser():
+
+    current_file = Path(__file__).resolve()
+    root_dir = current_file.parent
+    ignore_files = ['.git','__pycache__','volume']
+    markdown_content = generate_markdown(directory=root_dir,ignore_files=ignore_files)
+
+    print(markdown_content)
+    with open("README.md", "w") as md_file:
+        md_file.write("# Sandbox\n\n")
+        md_file.write('''Sandbox is collection of completely random codes that almost all the time does not inter-relate with each other and are standalone random scripts that I use to learn an concept\n\n''')
+        md_file.write("\n".join(markdown_content))
+
+
+
+if __name__ == '__main__':
+    traverser()
